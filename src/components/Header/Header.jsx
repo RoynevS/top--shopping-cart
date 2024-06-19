cd52aad7bfde5ab8ec1efd46448802175a862206
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-19 21:05:38 +0200

    Display amount of items in the cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ itemsInCart }) => {
   return (
     <header className={styles.header}>
       <nav>
@@ -15,10 +16,17 @@ const Header = () => {
           <li>
             <Link to="/cart">Cart</Link>
           </li>
+          <li>
+            <span>{itemsInCart.length || null}</span>
+          </li>
         </ul>
       </nav>
     </header>
   );
+};
+
+Header.propTypes = {
+  itemsInCart: PropTypes.array,
 };
 
 export default Header;
