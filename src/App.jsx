1fea7217a195ffbf341256af745161784bfc6bc9
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-19 12:40:38 +0200

    Render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useParams, useState } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import HomePage from "./routes/HomePage/HomePage";
+import Products from "./routes/Products/Products";
+import Cart from "./routes/Cart/Cart";
+
 function App() {
-  return;
+  const { site } = useParams();
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  return (
+    <>
+      <Header itemsInCart={itemsInCart} />
+      {site === "products" ? (
+        <Products setItems={setItemsInCart} />
+      ) : site === "cart" ? (
+        <Cart itemsInCart={itemsInCart} setItems={setItemsInCart} />
+      ) : (
+        <HomePage />
+      )}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
