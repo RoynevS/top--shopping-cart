032a74933dcb19df87a2fdca83d52b6629ed8bfe
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-19 21:06:32 +0200

    Display list of available products

--- src/routes/Products/Products.jsx
+++ src/routes/Products/Products.jsx
@@ -1,5 +1,44 @@
-const Products = () => {
-  return <h2>Products</h2>;
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import Product from "../../components/Product/Product";
+
+const Products = ({ setItems }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await fetch("https://fakestoreapi.com/products");
+      const products = await data.json();
+      console.log(products);
+
+      setProducts(products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <main>
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          src={product.image}
+          rating={product.rating.rate}
+          count={product.rating.count}
+          title={product.title}
+          description={product.description}
+          category={product.category}
+          price={product.price}
+          onClick={setItems}
+        />
+      ))}
+    </main>
+  );
+};
+
+Products.propTypes = {
+  setItems: PropTypes.func,
 };
 
 export default Products;
