25a2fcc828bdb27531274750257a50bd49daa610
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-20 13:32:23 +0200

    Display products and total

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,5 +1,40 @@
-const Cart = () => {
-  return <h2>Cart</h2>;
+import PropTypes from "prop-types";
+import Product from "../../components/Product/Product";
+import Button from "../../components/Button/Button";
+
+const Cart = ({ itemsInCart, setItemsInCart, setItems }) => {
+  const deleteProduct = (id) => {
+    const newProducts = itemsInCart.filter((product) => product.id !== id);
+    setItemsInCart([...newProducts]);
+  };
+
+  const calculateTotal = () => {
+    return itemsInCart
+      .map((product) => product.price * product.amount)
+      .reduce((acc, cur) => acc + cur, 0);
+  };
+
+  return (
+    <main>
+      {itemsInCart.map((product) => (
+        <Product
+          key={product.id}
+          product={product}
+          onClick={setItems}
+          onDelete={deleteProduct}
+          status="cart"
+        />
+      ))}
+      <p>{calculateTotal()}</p>
+      <Button text="Buy Products" onClick={() => alert(calculateTotal())} />
+    </main>
+  );
+};
+
+Cart.propTypes = {
+  itemsInCart: PropTypes.array,
+  setItemsInCart: PropTypes.func,
+  setItems: PropTypes.func,
 };
 
 export default Cart;
