ff865dc8c7d44c3c4dbf4875a1ed31a632a23487
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-20 13:31:56 +0200

    Render component different when in cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,20 +1,18 @@
 import PropTypes from "prop-types";
 import styles from "./Product.module.css";
 import { useState } from "react";
+import Button from "../Button/Button";
 
-const Product = ({
-  // id,
-  // src,
-  // rating,
-  // count,
-  // title,
-  // description,
-  // category,
-  // price,
-  product,
-  onClick,
-}) => {
-  const [amount, setAmount] = useState(1);
+const Product = ({ product, onClick, onDelete, status = "product" }) => {
+  // if product is in cart it has an amount so we set the amount to that
+  const [amount, setAmount] = useState(product.amount || 1);
+
+  const onChangeInput = (value) => {
+    if (typeof value === "number" || typeof value === "string")
+      setAmount(parseInt(value));
+    if (status === "cart") onClick(product, value, status);
+  };
+
   return (
     <div>
       <div>
@@ -23,42 +21,76 @@ const Product = ({
           alt={`Picture of ${product.title}`}
           className={styles.productImage}
         />
-        <div className={styles.ratingWrapper}>
-          <p className={styles.productRating}>Rating: {product.rating.rate}</p>
-          <p className={styles.productCount}>{product.rating.count}</p>
-        </div>
+        {status === "product" && (
+          <div className={styles.ratingWrapper}>
+            <p className={styles.productRating}>
+              Rating: {product.rating.rate}
+            </p>
+            <p className={styles.productCount}>{product.rating.count}</p>
+          </div>
+        )}
       </div>
       <div className={styles.descriptionWrapper}>
         <h2 className={styles.productTitle}>{product.title}</h2>
-        <p className={styles.productDescription}>{product.description}</p>
-        <p className={styles.productCategory}>{product.category}</p>
+        {status === "product" && (
+          <>
+            <p className={styles.productDescription}>{product.description}</p>
+            <p className={styles.productCategory}>{product.category}</p>
+          </>
+        )}
       </div>
       <div className={styles.priceWrapper}>
         <p className={styles.productPrice}>{product.price}</p>
-        <button onClick={() => amount > 1 && setAmount(amount - 1)}>-</button>
+        {status === "product" ? (
+          <Button
+            text="-"
+            onClick={() => amount > 1 && setAmount(amount - 1)}
+          />
+        ) : (
+          <Button
+            text="-"
+            onClick={() => {
+              amount > 1 && setAmount((prevAmount) => prevAmount - 1);
+              onClick(product, amount - 1, status);
+            }}
+          />
+        )}
+
         <input
           type="number"
           value={amount}
-          onChange={(event) => setAmount(event.target.value)}
+          onChange={(event) => onChangeInput(event.target.value)}
         />
-        <button onClick={() => setAmount(amount + 1)}>+</button>
-        <button onClick={() => onClick(product, amount)}>Add to Cart</button>
+        {status === "product" ? (
+          <Button text="+" onClick={() => setAmount(amount + 1)} />
+        ) : (
+          <Button
+            text="+"
+            onClick={() => {
+              setAmount((prevAmount) => prevAmount + 1);
+              onClick(product, amount + 1, status);
+            }}
+          />
+        )}
+
+        {status === "product" ? (
+          <Button
+            text="Add to Cart"
+            onClick={() => onClick(product, amount, status)}
+          />
+        ) : (
+          <Button text="Delete" onClick={() => onDelete(product.id)} />
+        )}
       </div>
     </div>
   );
 };
 
 Product.propTypes = {
-  // id: PropTypes.number,
-  // src: PropTypes.string,
-  // rating: PropTypes.number,
-  // count: PropTypes.number,
-  // title: PropTypes.string,
-  // description: PropTypes.string,
-  // category: PropTypes.string,
-  // price: PropTypes.number,
   product: PropTypes.object,
   onClick: PropTypes.func,
+  onDelete: PropTypes.func,
+  status: PropTypes.string,
 };
 
 export default Product;
