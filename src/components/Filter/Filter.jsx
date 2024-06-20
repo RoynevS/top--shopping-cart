eeb53fc1811cafd9a4ff375c82f2087cc1350def
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-20 18:44:58 +0200

    Add filter functionality

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,76 @@
-const Filter = () => {
-  return;
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const Filter = ({ filterProducts, categories, filter }) => {
+  const [search, setSearch] = useState("");
+  const [sliderValue, setSliderValue] = useState(1000);
+
+  const changeSearchHandler = (event) => {
+    setSearch(event.target.value);
+    filterProducts({ ...filter, title: event.target.value });
+  };
+
+  const changeCategoryHandler = (event) => {
+    filterProducts({ ...filter, category: event.target.value });
+  };
+
+  const changePriceHandler = (event) => {
+    setSliderValue(parseInt(event.target.value));
+    filterProducts({ ...filter, price: parseInt(event.target.value) });
+  };
+
+  return (
+    <aside>
+      <form>
+        <div>
+          <label htmlFor="search">Search</label>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            value={search}
+            onChange={changeSearchHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="category">Category</label>
+          <select
+            name="category"
+            id="category"
+            defaultValue="all"
+            onChange={changeCategoryHandler}
+          >
+            <option value="all">All</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="price-slider">Price</label>
+          <input
+            type="range"
+            name="price-slider"
+            id="price-slider"
+            value={sliderValue}
+            onChange={changePriceHandler}
+            min={0}
+            max={1000}
+            step={10}
+          />
+          <output>{sliderValue}</output>
+        </div>
+      </form>
+    </aside>
+  );
+};
+
+Filter.propTypes = {
+  filterProducts: PropTypes.func,
+  categories: PropTypes.array,
+  filter: PropTypes.object,
 };
 
 export default Filter;
--- src/routes/Products/Products.jsx
+++ src/routes/Products/Products.jsx
@@ -1,31 +1,58 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Product from "../../components/Product/Product";
+import Filter from "../../components/Filter/Filter";
 
 const Products = ({ setItems }) => {
   const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const [filter, setFilter] = useState({
+    title: "",
+    category: "all",
+    price: 1000,
+  });
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await fetch("https://fakestoreapi.com/products");
-      const products = await data.json();
-
-      setProducts(products);
-      setIsLoading(false);
-    };
-
-    fetchProducts();
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((products) => setProducts(products));
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((data) => data.json())
+      .then((categories) => setCategories(categories));
   }, []);
 
-  if (isLoading) return <h2>Loading...</h2>;
+  const filterProducts = (filterObj) => {
+    setFilter(filterObj);
+  };
+
+  const renderFilteredProducts = () => {
+    const displayedProducts = products
+      .filter((product) => product.title.toLowerCase().includes(filter.title))
+      .filter((product) => {
+        if (filter.category === "all") return true;
+        return product.category === filter.category;
+      })
+      .filter((product) => product.price <= filter.price);
+
+    return displayedProducts;
+  };
+
+  if (products.length === 0 || categories.length === 0)
+    return <h2>Loading...</h2>;
 
   return (
-    <main>
-      {products.map((product) => (
-        <Product key={product.id} product={product} onClick={setItems} />
-      ))}
-    </main>
+    <>
+      <Filter
+        filterProducts={filterProducts}
+        categories={categories}
+        filter={filter}
+      />
+      <main>
+        {renderFilteredProducts().map((product) => (
+          <Product key={product.id} product={product} onClick={setItems} />
+        ))}
+      </main>
+    </>
   );
 };
 
