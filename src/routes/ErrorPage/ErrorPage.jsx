21a765d1e0c34ac0ac6b9bfd161374caee78ee9a
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-22 11:08:00 +0200

    Add Error page for bad routes

--- src/routes/ErrorPage/ErrorPage.jsx
+++ src/routes/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
     <main>
       <h3>Page not found</h3>
+      <Link to="/">Go to Home page</Link>
     </main>
   );
 };
