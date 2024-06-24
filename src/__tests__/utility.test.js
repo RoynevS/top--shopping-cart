b02f253f1f582c58cdc71a722a27ddb35820752b
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-06-24 10:33:31 +0200

    Create test for utility functions

---
+++ src/__tests__/utility.test.js
@@ -0,0 +1,33 @@
+import { calculateTotal, toTitleCase } from "../utility/utility";
+
+describe("toTitleCase utility function", () => {
+  it("works with all lowercase string", () => {
+    expect(toTitleCase("hello")).toBe("Hello");
+  });
+
+  it("works with camelCase string", () => {
+    expect(toTitleCase("helloWorld")).toBe("Helloworld");
+  });
+
+  it("only title cases the first word in a sentence", () => {
+    expect(toTitleCase("this is just a tribute")).toBe(
+      "This is just a tribute"
+    );
+  });
+  it("works when string starts with whitespace", () => {
+    expect(toTitleCase("     hello")).toBe("Hello");
+  });
+});
+
+describe("calculateTotal utility function", () => {
+  const items = [
+    { price: 15, amount: 2 },
+    { price: 48.89, amount: 6 },
+    { price: 109.95, amount: 1 },
+    { price: 55.99, amount: 3 },
+  ];
+
+  it("calculates the right total", () => {
+    expect(calculateTotal(items)).toBe("601.26");
+  });
+});
